37dee42c8ea7cd778dba4f969069c34c50df8c1c
DhruvSaboo <saboodhruv789@gmail.com>
2025-07-03 22:02:58 +0530

    env sorted

--- src/main/java/com/example/ecommerce_spring/configuration/RetrofitConfig.java
+++ src/main/java/com/example/ecommerce_spring/configuration/RetrofitConfig.java
@@ -2,23 +2,27 @@ package com.example.ecommerce_spring.configuration;
 
 import com.example.ecommerce_spring.gateway.api.IFakeStoreCategoryApi;
 import com.example.ecommerce_spring.gateway.api.IFakeStoreProductAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.Objects;
 
 @Configuration
 public class RetrofitConfig {
 
-//    @Value("${FAKESTORE-BASEURL}")
-    private final String baseurl = "https://fakestoreapi.in/api/";
+    @Autowired
+    private Environment env;
 
     @Bean
     public Retrofit retroFit() {
-
+        String baseurl = env.getProperty("FAKESTORE-BASEURL");
         return new Retrofit.Builder()
-                .baseUrl(baseurl)
+                .baseUrl(Objects.requireNonNull(baseurl))
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
